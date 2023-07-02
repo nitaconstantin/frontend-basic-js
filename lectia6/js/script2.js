a9725484ab3fccc83cce72c5d18a3099469a85fc
Nita Constantin <ctinnita88@gmail.com>
2023-07-02 13:18:41 +0300

    lectia 6 - javascript-consola

---
+++ lectia6/js/script2.js
@@ -0,0 +1,24 @@
+"use strict";
+console.log("descriere...");
+// console.error("mesaj de eroare");
+// console.info("mesaj informativ");
+
+// comentariu pe o singura linie
+
+/*
+Comentariu
+pe 
+mai 
+multe
+linii
+*/
+
+// var - cea mai slaba declaratie a unei variabile
+// let - variabila careia ii putem modifica valoarea
+// const - variabila careia nu ii putem modifica valoarea si trebuie initializata cu tot cu valoare
+
+const x = 3;
+// x = 5;
+console.log("variabila x are valoarea: ", x);
+
+// let const noua modalitate de declarare a variabilelor
