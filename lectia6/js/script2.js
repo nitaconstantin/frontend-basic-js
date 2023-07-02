cb1135ab90436796364fe1980471c3970e52a75e
Nita Constantin <ctinnita88@gmail.com>
2023-07-02 14:57:29 +0300

    lectia 6 - Javascript - Tipuri de date

--- lectia6/js/script2.js
+++ lectia6/js/script2.js
@@ -17,8 +17,15 @@ linii
 // let - variabila careia ii putem modifica valoarea
 // const - variabila careia nu ii putem modifica valoarea si trebuie initializata cu tot cu valoare
 
-const x = 3;
+// let x = { name: "Andrei", age: 27 };
 // x = 5;
+// let x = function () {};
+let x = null;
 console.log("variabila x are valoarea: ", x);
 
 // let const noua modalitate de declarare a variabilelor
+
+// Tipuri de date primitive : numbers, strings, undefined, boolean, bigInt, symbol
+// Tipuri de date structurale: objects, functions
+// Tipul de date null
+console.log("variabila x este de tipul: ", typeof x);
