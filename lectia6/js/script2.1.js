777b5b61f5743c6614a6f3f0c9fdc32a54cb2fa5
Nita Constantin <ctinnita88@gmail.com>
2023-07-03 15:48:10 +0300

    lectia 6 -  Javascript - Structuri decizionale partea 2

--- lectia6/js/script2.1.js
+++ lectia6/js/script2.1.js
@@ -13,17 +13,22 @@ let b = 7;
 // console.log("Dupa if");
 
 // switch
-switch (a) {
-  case 3:
-    console.log("a are valoarea 3");
-    break;
-  case 5:
-    console.log("a are valoarea 5");
-    break;
-  case 7:
-    console.log("a are valoarea 7");
-    break;
-  default:
-    console.log("a nu are nici o valoare verificata");
-    break;
-}
+
+// switch (a) {
+//   case 3:
+//     console.log("a are valoarea 3");
+//     break;
+//   case 5:
+//     console.log("a are valoarea 5");
+//     break;
+//   case 7:
+//     console.log("a are valoarea 7");
+//     break;
+//   default:
+//     console.log("a nu are nici o valoare verificata");
+//     break;
+// }
+
+// ternary operator
+// NaN, undefined, null vor intoarce intotdeauna false
+undefined ? console.log("Adevarat!") : console.log("Fals!");
