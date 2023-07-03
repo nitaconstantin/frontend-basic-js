8386cfa176ce8bca4e803fce6f4103c7d533daec
Nita Constantin <ctinnita88@gmail.com>
2023-07-03 13:53:33 +0300

    lectia 6 - Javascript - Structuri decizionale partea 1

---
+++ lectia6/js/script2.1.js
@@ -0,0 +1,29 @@
+let a = 9;
+let b = 7;
+
+// console.log("Inainte de if");
+// if (a < 12) {
+//   // cod ce urmeaza a fi executat
+//   console.log("Da! expresia este adevarata!");
+// } else if (a > 3) {
+//   console.log("A doua expresie este adevarata");
+// } else {
+//   console.log("Toate expresiile sunt false!");
+// }
+// console.log("Dupa if");
+
+// switch
+switch (a) {
+  case 3:
+    console.log("a are valoarea 3");
+    break;
+  case 5:
+    console.log("a are valoarea 5");
+    break;
+  case 7:
+    console.log("a are valoarea 7");
+    break;
+  default:
+    console.log("a nu are nici o valoare verificata");
+    break;
+}
